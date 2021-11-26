4682f138f14f0558f63a2a752583d1ebedaeee2c
mt60000 <niruyaku45@gmail.com>
2021-11-26 14:50:02 +0900

    Chapter4 Finish

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,16 @@
+
+public class Chapter04 {
+	
+	public static void main(String[] args) {
+		String str1;
+		str1 = "小崎天生";
+		System.out.println(str1);
+		
+		str1 = "最後までチョコたっぷり！";
+		System.out.println(str1);
+		
+		int num1;
+		num1 = 666;
+		System.out.println(num1);
+	}
+}
