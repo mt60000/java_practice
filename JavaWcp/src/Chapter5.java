00dfc28a596e7e7e808ecb1cbc92c29eb24ac3a4
mt60000 <niruyaku45@gmail.com>
2021-11-26 15:29:19 +0900

    Chapter5 Finish

---
+++ JavaWcp/src/Chapter5.java
@@ -0,0 +1,27 @@
+
+public class Chapter5 {
+
+	public static void main(String[] args) {
+		long long1 = 298574930L;
+		double double1 = 1.99;
+		boolean boolean1 = true;
+		System.out.println(long1);
+		System.out.println(double1);
+		System.out.println(boolean1);
+		
+		Float Float1 = 3.15f;
+		Integer Integer1 = 45;
+		Character Character1 = 'も';
+		System.out.println(Float1);
+		System.out.println(Integer1);
+		System.out.println(Character1);
+		
+		int[] array1 = new int[5];
+		array1[0] = 50;
+		array1[1] = 30;
+		array1[2] = 4;
+		array1[3] = 6;
+		array1[4] = 5;
+		System.out.println(array1[3]);
+	}
+}
